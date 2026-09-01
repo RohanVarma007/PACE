d8671ead418f05f14a491fc9aafcca3ecb9b3b46
RohanVarma2007 <rohan.varma.buddaraju@gmail.com>
2026-09-01 13:58:09 +0530

    interactive chatbot added

--- server/Main.js
+++ server/Main.js
@@ -5,12 +5,19 @@ const axios = require('axios');
 const app = express();
 const splitModel = require('./splitschema.js');
 const runs = require('./routes.js');
+const chatbot = require('./chatbot.js');
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/runs', runs);
-
+app.post('/chat', async (req, res) => {
+    const userMessage = req.body.message;
+    console.log(userMessage);
+    const aiResponse = await chatbot.getChatResponse(userMessage);
+    console.log(aiResponse);
+    res.json({aiResponse});
+});
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
