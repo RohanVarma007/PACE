a5b0b886476eb75d05d8952090f3b8f5a25b6ea1
RohanVarma2007 <rohan.varma.buddaraju@gmail.com>
2026-09-15 19:14:32 +0530

    Minor issues resolved

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Runform from './Pages/Splits/splitd.jsx'
 import ChatBot from './Components/Chat/chat.jsx'
 import UserDatav from './Pages/UserData/userdatav.jsx'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter, Navigate, Outlet} from 'react-router-dom'
 import Pointer from './Components/Cursor/pointer.jsx'
 import Navigation from './Components/Navigation/nav.jsx'
 import Login from './Pages/signIn/login.jsx'
@@ -18,33 +18,40 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
 
-        {/* Protected routes (with nav and cursor) */}
         <Route
           element={
             isLoggedIn() ? (
               <>
                 <Navigation />
                 <Pointer />
+                <Outlet />
               </>
             ) : (
               <Navigate to="/login" replace />
             )
           }
         >
-          <Route exact path="/runform/:runId" element={<Coach />} />
-          <Route exact path="/userdata" element={<UserDatav />} />
-          <Route exact path="/runform" element={<Runform />} />
-          <Route exact path="/chat" element={<ChatBot />} />
+          <Route path="/runform/:runId" element={<Coach />} />
+          <Route path="/userdata" element={<UserDatav />} />
+          <Route path="/runform" element={<Runform />} />
+          <Route path="/chat" element={<ChatBot />} />
         </Route>
 
-        {/* Redirect root to login or userdata */}
-        <Route exact path="/" element={<Navigate to={isLoggedIn() ? "/userdata" : "/login"} replace />} />
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to={isLoggedIn() ? "/userdata" : "/login"}
+              replace
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
